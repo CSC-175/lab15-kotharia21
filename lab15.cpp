204b5ba3e71f5f64733cda77c524d665d5e90bbd
Amaan Kothari <kotharia@umich.edu>
2025-03-27 20:03:12 -0400

    L15 Attempt 1

--- lab15.cpp
+++ lab15.cpp
@@ -1,10 +1,46 @@
 #include<iostream>
 using namespace std;
 
+double factorial(int num){
+  if(num<=1)
+    return 1;
+  return num*factorial(num-1);
+
+}
+double computeWays(int n, int k){
+  return 1.0/(factorial(n)/(factorial(k)*factorial(n-k)));
+
+}
 // Function prototypes
-void getInfo(int&, int&);
-double computeWays(int, int);
-double factorial(int);
+void getInfo(int&n, int&k){
+  bool validInput;
+  do {
+    cout<<"How many balls (1-12) are in the pool to pick from? ";
+    cin>>n;
+    validInput = !cin.fail() && (n>=1 && n<=12);
+    if (!validInput) {
+      cout<<"Input Error!"<<endl;
+      cin.clear();
+      cin.ignore(10000,'\n');
+    }
+  } while (!validInput);
+  do {
+    cout<<"How many balls (1-" << n << ") will be drawn? ";
+    cin>>k;
+    validInput = !cin.fail() && (k>=1 && k<=n);
+    if (!validInput) {
+      cout<<"Input Error!"<<endl;
+      cin.clear();
+      cin.ignore(10000,'\n');
+    }
+  } while (!validInput);
+
+
+
+}
+
+
+
 
 /*******************************************************************
 * getLotteryInfo                                                   *
